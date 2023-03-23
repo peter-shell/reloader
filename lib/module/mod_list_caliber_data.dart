2e7c375a58aae8d065b243988df9c0cc59144b9f
peter-shell <71984315+peter-shell@users.noreply.github.com>
2023-03-22 18:14:02 -0700

    same

---
+++ lib/module/mod_list_caliber_data.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+List<DropdownMenuItem<String>> testMenuItems = [
+  const DropdownMenuItem(value: "None", child: Text("Select A Caliber")),
+  const DropdownMenuItem(value: ".17", child: Text(".17 / 4.4mm")),
+  const DropdownMenuItem(value: ".20", child: Text(".22 / 5.56mm")),
+  const DropdownMenuItem(value: ".243", child: Text(".243 / 6mm")),
+  const DropdownMenuItem(value: ".25", child: Text(".25 / 6.35")),
+  const DropdownMenuItem(value: ".260", child: Text(".260 / 6.5mm Depth")),
+  const DropdownMenuItem(value: ".270", child: Text(".270")),
+  const DropdownMenuItem(value: "Seating Depth", child: Text("Seating Depth")),
+  const DropdownMenuItem(value: "Seating Depth", child: Text("Seating Depth")),
+  const DropdownMenuItem(value: "Seating Depth", child: Text("Seating Depth")),
+  const DropdownMenuItem(value: "Seating Depth", child: Text("Seating Depth")),
+  const DropdownMenuItem(value: "Seating Depth", child: Text("Seating Depth")),
+  const DropdownMenuItem(value: "Seating Depth", child: Text("Seating Depth")),
+  const DropdownMenuItem(value: "Seating Depth", child: Text("Seating Depth")),
+];
