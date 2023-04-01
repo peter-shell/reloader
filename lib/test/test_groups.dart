eb44cb49d197457198ad82a744c2ae6675a89216
peter-shell <shellp@oregonstate.edu>
2023-04-01 13:13:51 -0700

    updated group_tests

--- lib/test/test_groups.dart
+++ lib/test/test_groups.dart
@@ -7,10 +7,10 @@ void main() {
   double velocity = 2700.0;
   double xpos = 10;
   double ypos = 10;
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < 5; i++) {
     myShots.add(Shot(velocity: velocity, xpos: xpos, ypos: ypos));
     velocity += 0;
-    xpos += 50.5;
+    xpos += 10.5;
     ypos += 75.2;
   }
   Group mygroup = Group(
@@ -25,17 +25,29 @@ void main() {
   mygroup.iconSize = 50;
   myShots.forEach((shot) => {
         mygroup.addShot(shot),
-        print("Num Shots: ${mygroup.numShots}"),
-        print(mygroup.ctcGroupSize.toString())
+
+        // print(
+        //     "avg x pos: ${mygroup.returnAverageXPosOfShots(mygroup.returnLowestPointInGroup("x"))}"),
+        //print(mygroup.ctcGroupSize.toString())
         // print("Average Velocity: ${mygroup.avgVelocity}"),
         // print("Standard Deviation: ${mygroup.standDeviation}"),
         // print("Extreme Spread ${mygroup.extremeSpread}"),
         // print("Max Velocity: ${mygroup.maxVelocity}"),
         // print("Min Velocity: ${mygroup.minVelocity}"),
       });
-  test("All shot objects were added", () => {expect(mygroup.shots.length, 6)});
+  test("All shot objects were added", () => {expect(mygroup.shots.length, 5)});
   test("first shot is correct speed",
       () => {expect(mygroup.shots[0].velocity, 2700.0)});
   test("test setting ctcSize",
       () => {mygroup.ctcGroupSize = .89, expect(mygroup.ctcGroupSize, .89)});
+  test(
+      "return center position of all xpos in group",
+      () => {
+            expect(
+                mygroup.returnAverageXPosOfShots(
+                    mygroup.returnLowestPointInGroup("x")),
+                36.25)
+          });
+  //mygroup.calculateGroupMeanRadius();
+  //print(mygroup.groupMeanRadius);
 }
