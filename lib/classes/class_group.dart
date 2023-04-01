38ed9c5373c575802bb327d2c33f6864122e5c00
peter-shell <shellp@oregonstate.edu>
2023-04-01 13:13:28 -0700

    completed groupmeanradius function

--- lib/classes/class_group.dart
+++ lib/classes/class_group.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 import 'dart:math';
+
+import 'package:collection/collection.dart';
 import 'class_shot.dart';
 
 class Group {
@@ -14,6 +16,8 @@ class Group {
   double bulletDiameter; // needs set by frontend
   double iconSize; // needs set by frontend
   String pathToImageOfGroup;
+  Shot? centerOfGroup;
+  double groupMeanRadius;
   Group(
       {required this.shots,
       required this.ctcGroupSize,
@@ -25,7 +29,9 @@ class Group {
       this.numShots = 0,
       this.bulletDiameter = 0.0,
       this.iconSize = 0.0,
-      this.pathToImageOfGroup = ""});
+      this.pathToImageOfGroup = "",
+      this.centerOfGroup,
+      this.groupMeanRadius = 0});
 
   factory Group.fromJson(Map<String, dynamic> data) {
     final List<Shot> tempshots = [];
@@ -44,6 +50,8 @@ class Group {
     final bulletDiameter = double.parse(data['bulletDiameter']);
     final iconSize = double.parse(data['iconSize']);
     final pathToImageOfGroup = data['pathToImageOfGroup'];
+    final centerOfGroup = data['centerOfGroup'];
+    final groupMeanRadius = data['groupMeanRadius'];
     return Group(
         shots: tempshots,
         ctcGroupSize: ctcGroupSize,
@@ -55,7 +63,9 @@ class Group {
         numShots: numShots,
         bulletDiameter: bulletDiameter,
         iconSize: iconSize,
-        pathToImageOfGroup: pathToImageOfGroup);
+        pathToImageOfGroup: pathToImageOfGroup,
+        centerOfGroup: centerOfGroup,
+        groupMeanRadius: groupMeanRadius);
   }
   Map<String, dynamic> toJson() => {
         "shots": shots == []
@@ -70,8 +80,11 @@ class Group {
         "numShots": numShots.toString(),
         "bulletDiameter": bulletDiameter.toStringAsFixed(3),
         "iconSize": iconSize.toStringAsFixed(2),
-        "pathToImageOfGroup": pathToImageOfGroup
+        "pathToImageOfGroup": pathToImageOfGroup,
+        "centerOfGroup": centerOfGroup,
+        "groupMeanRadius": groupMeanRadius
       };
+
   void calculateVelocityStuff() {
     double count = 0.0;
     double velocitySum = 0.0;
@@ -116,7 +129,94 @@ class Group {
     }
   }
 
-  void calculateGroupSize() {
+  void calculateGroupMeanRadius() {
+    // mean radius (litz recommends)
+    // find the mathematical center of the group you shot. Then you measure the distance of
+    //each individual shot from the mathematical center and average those distances together.
+    //to obtain the mean radius of a shot-group, measure the heights of all shots above the
+    //lowest shot in the group. Average these measurements. The result is the height of the
+    //center of the group above the lowest shot. Then in the same way, get the horizontal
+    //distance of the center from the shot farthest to the left. These two measurements will
+    //locate the group center. Now measure the distance of each shot from this center.
+    //The average of these measures is the mean radius.
+
+    double centerOfX = returnAverageXPosOfShots(returnLowestPointInGroup("x"));
+    double centerOfY = returnAverageYposOfShots(returnLowestPointInGroup("y"));
+    centerOfGroup = Shot(velocity: 0, xpos: centerOfX, ypos: centerOfY);
+    List<double> shotDistancesFromCenter = [];
+    for (int i = 0; i < shots.length; i++) {
+      shotDistancesFromCenter
+          .add(distanceBetweenTwoPoints(centerOfGroup!, shots[i]));
+    }
+    //print(shotDistancesFromCenter);
+    double averageShotDistanceFromCenter = shotDistancesFromCenter.average;
+    //print(averageShotDistanceFromCenter);
+    // convert to inches, round up, and return
+    double meanRadius = convertPixelsToInches(
+        averageShotDistanceFromCenter, bulletDiameter, iconSize);
+    //print(meanRadius);
+    groupMeanRadius = double.parse(meanRadius.toStringAsFixed(3));
+  }
+
+  double returnAverageXPosOfShots(int indexOfLowestPosition) {
+    // measure heights of all shots above the lowest, then average those measurements
+    // subtract lowest from every shot, add up, average
+    // then add the average back to the lowest shot for center position
+    double averageHeight = 0;
+    double lowestPosition = shots[indexOfLowestPosition].xpos;
+    for (int i = 0; i < shots.length; i++) {
+      if (i == indexOfLowestPosition) {
+        continue;
+      }
+      averageHeight += shots[i].xpos - lowestPosition;
+    }
+    double averageXposition = (averageHeight / (shots.length - 1)) +
+        shots[indexOfLowestPosition].xpos;
+    return double.parse(averageXposition.toStringAsFixed(3));
+  }
+
+  double returnAverageYposOfShots(int indexOfLowestPosition) {
+    //same as above
+    double averageHeight = 0;
+    double lowestPosition = shots[indexOfLowestPosition].ypos;
+    for (int i = 0; i < shots.length; i++) {
+      if (i == indexOfLowestPosition) {
+        continue;
+      }
+      averageHeight += shots[i].ypos - lowestPosition;
+    }
+    double averageYposition = (averageHeight / (shots.length - 1)) +
+        shots[indexOfLowestPosition].ypos;
+    return double.parse(averageYposition.toStringAsFixed(3));
+  }
+
+  // "x" returns index of lowest xpos, "y" returns index of lowest ypos
+  int returnLowestPointInGroup(String coordinatePlane) {
+    switch (coordinatePlane) {
+      case "x":
+        // find the lowest shot
+        int lowestPoint = 0;
+        // set first shot as lowest, compare to the rest
+        for (int i = 1; i < shots.length; i++) {
+          if (shots[i].xpos < shots[lowestPoint].xpos) {
+            lowestPoint = i;
+          }
+        }
+        return lowestPoint;
+      case "y":
+        int lowestPoint = 0;
+        for (int i = 1; i < shots.length; i++) {
+          if (shots[i].ypos < shots[lowestPoint].ypos) {
+            lowestPoint = i;
+          }
+        }
+        return lowestPoint;
+      default:
+        throw "returnLowestPointInGroup: misused function parameter";
+    }
+  }
+
+  void calculateGroupExtremeSpread() {
     // the parameter bulletDiameter is probably just temporary
     double greatestDistance = 0;
     for (int i = 0; i < shots.length; i++) {
@@ -129,8 +229,6 @@ class Group {
     double distanceInInches =
         convertPixelsToInches(greatestDistance, bulletDiameter, iconSize);
     // do some rounding on distanceInInches
-    double plusHalfBulletDiameter = distanceInInches;
-    // TODO: unsure about adding half here. It seems close on the sample, need to do some testing on this soon!
 
     ctcGroupSize = double.parse(distanceInInches.toStringAsFixed(3));
   }
@@ -160,7 +258,7 @@ class Group {
     }
     numShots += 1;
     if (shots.length > 1) {
-      calculateGroupSize();
+      calculateGroupExtremeSpread();
       // print(iconSize);
       // print(ctcGroupSize);
     }
@@ -170,7 +268,7 @@ class Group {
     shots.removeAt(0);
     numShots -= 1;
     if (shots.length > 1) {
-      calculateGroupSize();
+      calculateGroupExtremeSpread();
     }
     if (shots.length == 1) {
       ctcGroupSize = bulletDiameter;
@@ -189,6 +287,6 @@ class Group {
   }
 
   //TODO: add vertical and horizontal standard deviation
-  //TODO: add mean radius (litz recommends)
+
   //TODO: add circular error probable
 }
